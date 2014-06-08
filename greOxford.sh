9203ffa95593dd845a2c4bfa9319a7e511f0cfb3
Abhay Mittal <abhaymittal@y7mail.com>
2014-06-08 15:42:24 +0530

    Starting

---
+++ greOxford.sh
@@ -0,0 +1,5 @@
+#Bash Script which accepts a list of words and notifies on regular intervals (set by user) by picking a random word from the list and displaying its meaning along with some example sentences from Oxford dictionary website using American English.
+
+# Author: Abhay Mittal
+
+
